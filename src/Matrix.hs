b98f35ce48648353a5d96b07908e03b73a9d9fb6
Roman Venediktov <e2e4b6b7@gmail.com>
2021-09-20 02:02:39 +0300

    Style fixes

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -47,34 +47,34 @@ infixl 6 +#
 
 infixl 7 *#
 
-(<#) :: (Show a, Eq a, Semiring a) => Matrix a -> (a, Int, Int) -> Matrix a
+(<#) :: (Eq a, Semiring a) => Matrix a -> (a, Int, Int) -> Matrix a
 matrix <# (val, w, h) = setVal matrix val w h
 
-(<<#) :: (Show a, Eq a, Semiring a) => Matrix a -> [(a, Int, Int)] -> Matrix a
+(<<#) :: (Eq a, Semiring a) => Matrix a -> [(a, Int, Int)] -> Matrix a
 (<<#) = foldl (<#)
 
-(*#) :: (Show a, Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
+(*#) :: (Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
 (*#) = matrixMultiplication
 
-(+#) :: (Show a, Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
+(+#) :: (Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
 (+#) = matrixSum
 
-leaf :: (Show a, Eq a, Semiring a) => a -> MatrixValues a
+leaf :: (Eq a, Semiring a) => a -> MatrixValues a
 leaf val
   | val == zero = Zero
   | otherwise = Leaf val
 
-nonZero :: (Show a, Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a -> MatrixValues a -> MatrixValues a
+nonZero :: (Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a -> MatrixValues a -> MatrixValues a
 nonZero ul ur dl dr
   | ul == Zero && ur == Zero && dl == Zero && dr == Zero = Zero
   | otherwise = NonZero ul ur dl dr
 
-nonZeroH :: (Show a, Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
+nonZeroH :: (Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
 nonZeroH l r
   | l == Zero && r == Zero = Zero
   | otherwise = NonZeroH l r
 
-nonZeroV :: (Show a, Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
+nonZeroV :: (Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
 nonZeroV u d
   | u == Zero && d == Zero = Zero
   | otherwise = NonZeroV u d
@@ -82,43 +82,45 @@ nonZeroV u d
 emptyMatrix :: Int -> Int -> Matrix a
 emptyMatrix w h = Matrix w h Zero
 
-setVal :: (Show a, Eq a, Semiring a) => Matrix a -> a -> Int -> Int -> Matrix a
+setVal :: (Eq a, Semiring a) => Matrix a -> a -> Int -> Int -> Matrix a
 setVal matrix@(Matrix width height values) val w h
   | w < 0 || w >= width || h < 0 || h >= height = error "Position not in matrix"
   | otherwise = matrix {values = setValValues values val w h width height}
 
-setValValues :: (Show a, Eq a, Semiring a) => MatrixValues a -> a -> Int -> Int -> Int -> Int -> MatrixValues a
-setValValues values@(NonZero ul ur dl dr) val w h width height
+setValValues :: (Eq a, Semiring a) => MatrixValues a -> a -> Int -> Int -> Int -> Int -> MatrixValues a
+setValValues values@(NonZero ul ur dl dr) val w h submatrixWidth submatrixHeight
   | w < midW && h < midH = values {ul = setValValues ul val w h midW midH}
-  | w >= midW && h < midH = values {ur = setValValues ur val (w - midW) h (width - midW) midH}
-  | w < midW && h >= midH = values {dl = setValValues dl val w (h - midH) midW (height - midH)}
-  | w >= midW && h >= midH = values {dr = setValValues dr val (w - midW) (h - midH) (width - midW) (height - midH)}
+  | w >= midW && h < midH = values {ur = setValValues ur val (w - midW) h (submatrixWidth - midW) midH}
+  | w < midW && h >= midH = values {dl = setValValues dl val w (h - midH) midW (submatrixHeight - midH)}
+  | w >= midW && h >= midH =
+    values {dr = setValValues dr val (w - midW) (h - midH) (submatrixWidth - midW) (submatrixHeight - midH)}
   where
-    midW = width `div` 2
-    midH = height `div` 2
-setValValues values@(NonZeroV u d) val 0 h 1 height
+    midW = submatrixWidth `div` 2
+    midH = submatrixHeight `div` 2
+setValValues values@(NonZeroV u d) val 0 h 1 submatrixHeight
   | h < midH = values {u = setValValues u val 0 h 1 midH}
-  | h >= midH = values {d = setValValues d val 0 (h - midH) 1 (height - midH)}
+  | h >= midH = values {d = setValValues d val 0 (h - midH) 1 (submatrixHeight - midH)}
   where
-    midH = height `div` 2
-setValValues values@(NonZeroH l r) val w 0 width 1
+    midH = submatrixHeight `div` 2
+setValValues values@(NonZeroH l r) val w 0 submatrixWidth 1
   | w < midW = values {l = setValValues l val w 0 midW 1}
-  | w >= midW = values {r = setValValues r val (w - midW) 0 (width - midW) 1}
+  | w >= midW = values {r = setValValues r val (w - midW) 0 (submatrixWidth - midW) 1}
   where
-    midW = width `div` 2
+    midW = submatrixWidth `div` 2
 setValValues Zero val 0 0 1 1 = Leaf val
-setValValues Zero val 0 h 1 height = setValValues (NonZeroV Zero Zero) val 0 h 1 height
-setValValues Zero val w 0 width 1 = setValValues (NonZeroH Zero Zero) val w 0 width 1
-setValValues Zero val w h width height = setValValues (NonZero Zero Zero Zero Zero) val w h width height
+setValValues Zero val 0 h 1 submatrixHeight = setValValues (NonZeroV Zero Zero) val 0 h 1 submatrixHeight
+setValValues Zero val w 0 submatrixWidth 1 = setValValues (NonZeroH Zero Zero) val w 0 submatrixWidth 1
+setValValues Zero val w h submatrixWidth submatrixHeight =
+  setValValues (NonZero Zero Zero Zero Zero) val w h submatrixWidth submatrixHeight
 setValValues (Leaf _) val 0 0 1 1 = Leaf val
 setValValues _ _ _ _ _ _ = error "Unexpected matrix structure"
 
-matrixSum :: (Show a, Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
+matrixSum :: (Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
 matrixSum (Matrix w1 h1 values1) (Matrix w2 h2 values2)
   | w1 /= w2 || h1 /= h2 = error "Matrices are not the same size"
   | otherwise = Matrix w1 h1 $ matrixSumValues values1 values2
 
-matrixSumValues :: (Show a, Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
+matrixSumValues :: (Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
 matrixSumValues a Zero = a
 matrixSumValues Zero a = a
 matrixSumValues (Leaf a) (Leaf b) = leaf $ a `plus` b
@@ -138,12 +140,12 @@ matrixSumValues (NonZeroH u1 d1) (NonZeroV u2 d2) =
     (matrixSumValues d1 d2)
 matrixSumValues _ _ = error "Mismatch of matrix structures"
 
-matrixMultiplication :: (Show a, Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
+matrixMultiplication :: (Eq a, Semiring a) => Matrix a -> Matrix a -> Matrix a
 matrixMultiplication (Matrix w1 h1 values1) (Matrix w2 h2 values2)
   | w1 /= h2 = error "Matrix sizes are not suitable for multiplication"
   | otherwise = Matrix w2 h1 $ matrixMultiplicationValues values1 values2
 
-matrixMultiplicationValues :: (Show a, Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
+matrixMultiplicationValues :: (Eq a, Semiring a) => MatrixValues a -> MatrixValues a -> MatrixValues a
 matrixMultiplicationValues _ Zero = Zero
 matrixMultiplicationValues Zero _ = Zero
 matrixMultiplicationValues (Leaf a) (Leaf b) = leaf $ a `times` b
